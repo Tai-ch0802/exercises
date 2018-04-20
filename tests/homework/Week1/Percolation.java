b3e35d77cb4367e0b442321f191676bdb05eec70
Tai-chunchien <i0927935851@gmail.com>
2018-04-20 11:45:45 +0800

    Adjust homework

--- tests/homework/Week1/Percolation.java
+++ tests/homework/Week1/Percolation.java
@@ -1,6 +1,6 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+//import edu.princeton.cs.algs4.StdRandom;
+//import edu.princeton.cs.algs4.StdStats;
+//import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import java.lang.IllegalArgumentException;
 
 public class Percolation {
@@ -27,53 +27,66 @@ public class Percolation {
         if (row <= 0 || col <= 0) {
             throw new IllegalArgumentException();
         }
-
-        if (this.target[row-1][col-1] !== BLOCKED_SITE) {
+        if (this.target[row-1][col-1] != BLOCKED_SITE) {
             return;
         }
 
         if (
-            col === 1 ||
-            this.target[row-2][col-1] === FULL_OPEN_SITE ||
-            this.target[row][col-1] === FULL_OPEN_SITE ||
-            this.target[row-1][col-2] === FULL_OPEN_SITE ||
-            this.target[row-1][col] === FULL_OPEN_SITE ||
+            col == 1 ||
+            this.target[row-2][col-1] == FULL_OPEN_SITE ||
+            this.target[row][col-1] == FULL_OPEN_SITE ||
+            this.target[row-1][col-2] == FULL_OPEN_SITE ||
+            this.target[row-1][col] == FULL_OPEN_SITE
         ) {
-            this.target[row-1][col-1] = FULL_OPEN_SITE;
+            this.fillWater(row, col);
             this.countOfOpenSites++;
             return;
         }
-        this.target[row-1][col-1] = 1;
+
+        this.target[row-1][col-1] = EMPTY_OPEN_SITE;
         this.countOfOpenSites++;
     }   // open site (row, col) if it is not open already
+
+    private int fillWater(int row, int col)
+    {
+        this.target[row-1][col-1] = FULL_OPEN_SITE;
+
+        this.target[row-2][col-1] = this.target[row-2][col-1] == EMPTY_OPEN_SITE ? this.fillWater(row-2, col-1) : this.target[row-2][col-1];
+        this.target[row][col-1] = this.target[row][col-1] == EMPTY_OPEN_SITE ? this.fillWater(row, col-1) : this.target[row-2][col-1];
+        this.target[row-1][col-2] = this.target[row-1][col-2] == EMPTY_OPEN_SITE ? this.fillWater(row-1, col-2) : this.target[row-2][col-1];
+        this.target[row-1][col] = this.target[row-1][col] == EMPTY_OPEN_SITE ? this.fillWater(row-1, col) : this.target[row-2][col-1];
+
+        return FULL_OPEN_SITE;
+    }
 
     public boolean isOpen(int row, int col) {
         if (row <= 0 || col <= 0) {
             throw new IllegalArgumentException();
         }
-        return this.target[row-1][col-1] !== BLOCKED_SITE;
+        return this.target[row-1][col-1] != BLOCKED_SITE;
     }  // is site (row, col) open?
 
     public boolean isFull(int row, int col) {
         if (row <= 0 || col <= 0) {
             throw new IllegalArgumentException();
         }
-        return this.target[row-1][col-1] !== FULL_OPEN_SITE;
+        return this.target[row-1][col-1] != FULL_OPEN_SITE;
     } // is site (row, col) full?
-    
+
     public int numberOfOpenSites() {
         return this.countOfOpenSites;
     }       // number of open sites
 
-   public boolean percolates() {
-        for (int i = 0; i < this.width; $i++) {
-            if (this.target[this.width-1][i] === FULL_OPEN_SITE) {
+    public boolean percolates() {
+        for (int i = 0; i < this.width; i++) {
+            if (this.target[this.width-1][i] == FULL_OPEN_SITE) {
                 return true;
             }
         }
         return false;
-   }  // does the system percolate?
+    }  // does the system percolate?
 
-   public static void main(String[] args) {
-   }   // test client (optional)
+    public static void main(String[] args) {
+        
+    }   // test client (optional)
 }
